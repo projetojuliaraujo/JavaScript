2a30aeebcaf61389c7588796b5343379821155bf
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2023-09-12 10:33:06 -0300

    Add files via upload

--- IMC.JS/imc.js
+++ IMC.JS/imc.js
@@ -7,19 +7,19 @@ function calcula_imc(){
     var calculo = (peso/(altura*altura));
 
     if(calculo<18.5){
-    alert("Você está magro com esse índice: " + calculo);
+    alert("Você está abaixo do peso: " + calculo);
     }
     else if(calculo>=18.5 && calculo<24.9){
-    alert("Você está normal com esse índice: " + calculo);    
+    alert("Você está em seu peso normal: " + calculo);    
     }
     else if(calculo>=25 && calculo<29.9){
-    alert("Você está com sobre peso com esse índice: " + calculo);    
+    alert("Você está com sobrepeso: " + calculo);    
     }
     else if(calculo>=30 && calculo<39.9){
-    alert("Você está com obesidade com esse índice: " + calculo);
+    alert("Você está com obesidade: " + calculo);
     }
     else if(calculo>40){
-    alert("Você está com obesidade grave com essa índice: " + calculo);    
+    alert("Você está com obesidade grave: " + calculo);    
     }
 
     }
--- IMC.JS/imc2.js
+++ IMC.JS/imc2.js
@@ -7,19 +7,19 @@ function calcula_imc(){
     var calculo = (peso/(altura*altura));
 
     if(calculo<18.5){
-    resultado = "Você está magro com esse índice: " + calculo
+    resultado = "Você está abaixo do peso: " + calculo
     }
     else if(calculo>=18.5 && calculo<24.9){
-    resultado = "Você está normal com esse índice: " + calculo   
+    resultado = "Você está em seu peso normal: " + calculo   
     }
     else if(calculo>=25 && calculo<29.9){
-    resultado = "Você está com sobre peso com esse índice: " + calculo    
+    resultado = "Você está com sobrepeso: " + calculo    
     }
     else if(calculo>=30 && calculo<39.9){
-    resultado = "Você está com obesidade com esse índice: " + calculo
+    resultado = "Você está com obesidade: " + calculo
     }
     else if(calculo>40){
-    resultado = "Você está com obesidade grave com essa índice: " + calculo    
+    resultado = "Você está com obesidade grave: " + calculo    
     }
 
     document.getElementById('resul').innerHTML = resultado;
